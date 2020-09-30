50cb2f16d0f955270a4c9577b88dd364c0313626
Minhaz <minhaz725@gmail.com>
2020-10-01 02:40:16 +0600

    sem value retrieved(10+ h) :)

--- Cycle.cpp
+++ Cycle.cpp
@@ -8,28 +8,40 @@
 
 using namespace std;
 
-#define number_of_producers 5
-#define arr_size 5
+#define number_of_serviceman 1
+#define number_of_cycles 1
+#define arr_size 3 //service room size 
 //test auto stage
 //g++ -D_REENTRANT Cycle.cpp -o Cycle -lpthread
 //./Cycle
 int ind=1;
+
 pthread_mutex_t mutex;
 sem_t full_array, empty_array;
+char* sharedmemory[arr_size]; 
+int value;
 
 void* produce_item(void* arg)
 {
 	//printf("I'm producer %s\n",(char*)arg);
 	//pthread_exit((void*)strcat((char*)arg," is finishing\n"));
+	
 	while(true)
 	{
+		sem_getvalue(&empty_array,&value);
 		sem_wait(&empty_array); //down
+
 		pthread_mutex_lock(&mutex);
+		
+		sharedmemory[0]=(char*)arg;
 		sleep(1);
-		printf("producer %s has produced %d th item\n",(char*)arg,ind);
+		printf("producer %s has produced %d th item\n",(char*)arg,value);
 		ind++;
 		pthread_mutex_unlock(&mutex);
 		sem_post(&full_array); //up
+		//if(ind >3 ) ind=1;
+
+
 	}
 }
 
@@ -37,12 +49,20 @@ void* consume_item(void* arg)
 {
 	//printf("I'm producer %s\n",(char*)arg);
 	//pthread_exit((void*)strcat((char*)arg," is finishing\n"));
+	//char* priority[arr_size];
+	//priority[0]=(char*)1;
 	while(true)
 	{
+		//reseter=ind;
+		//if(priority[0]==sharedmemory[0]) continue;
+		sem_getvalue(&full_array,&value);
 		sem_wait(&full_array); //down
+		
 		pthread_mutex_lock(&mutex);
-		sleep(1);
-		printf("consumer %s has consumed %d th item\n",(char*)arg,ind);
+		printf("cyclist %s started taking service from serviceman %d\n",(char*)arg,value);
+	
+		sleep(5);
+		printf("cyclist %s finished taking service from serviceman %d\n",(char*)arg,value);
 		pthread_mutex_unlock(&mutex);
 		sem_post(&empty_array); //up
 	}
@@ -53,30 +73,64 @@ int main()
 	int res;
 
 	res= sem_init(&full_array,0,0);
+	if (res != 0)
+		{
+			printf("Failed!\n");
+		}
 	res= sem_init(&empty_array,0,arr_size);
+	if (res != 0)
+		{
+			printf("Failed!\n");
+		}
 
 
 	pthread_mutex_init(&mutex, NULL);
 
 
-	pthread_t producers[number_of_producers];
+	pthread_t serviceman[number_of_serviceman];
 
-	for (int i = 0; i < number_of_producers; ++i)
+	for (int i = 0; i < number_of_serviceman; ++i)
 	{
 		char* id = new char[3];
 		strcpy(id , to_string(i+1).c_str());
-		res = pthread_create(&producers[i],NULL,produce_item,id);
-		pthread_create(&producers[i],NULL,consume_item,id);
+		res = pthread_create(&serviceman[i],NULL,produce_item,id);
+		
 		if (res != 0)
 		{
 			printf("Failed!\n");
 		}
 	}
 
-	for (int i = 0; i < number_of_producers; ++i)
+	pthread_t cycles[number_of_cycles];
+
+	for (int i = 0; i < number_of_cycles; ++i)
+	{
+		char* id = new char[3];
+		strcpy(id , to_string(i+1).c_str());
+		res = pthread_create(&cycles[i],NULL,consume_item,id);
+		if (res != 0)
+		{
+			printf("Failed!\n");
+		}
+	}
+
+
+
+	
+
+
+
+	for (int i = 0; i < number_of_cycles; ++i)
 	{
 		void* result;
-		pthread_join(producers[i],&result);
+		pthread_join(cycles[i],&result);
+		printf("%s", (char*)result);
+	}
+	
+	for (int i = 0; i < number_of_serviceman; ++i)
+	{
+		void* result;
+		pthread_join(serviceman[i],&result);
 		printf("%s", (char*)result);
 	}
 	//cout<<"hello"<<endl;
